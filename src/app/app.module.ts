927c600ce8aee86babfb0c66681c50331f42479c
unknown <seoyoonkg@naver.com>
2017-09-27 17:32:48 +0900

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { Geolocation } from '@ionic-native/geolocation';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { NativeGeocoder } from '@ionic-native/native-geocoder';
+import { IonicStorageModule } from '@ionic/storage';
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +15,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +26,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
+    NativeGeocoder,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,111 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,AlertController,LoadingController  } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { NativeGeocoder, NativeGeocoderReverseResult } from '@ionic-native/native-geocoder';
+import { Storage } from '@ionic/storage';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  items:any[] = [];
+  lat;
+  lng;
+  loading;
+  constructor(public navCtrl: NavController, private geolocation: Geolocation, private alertCtrl:AlertController, 
+    private nativeGeocoder:NativeGeocoder, public storage:Storage ,public loadingCtrl: LoadingController) {
+    this.loadData();
   }
+  showLoading()
+  {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    this.loading.present();
+  }
+  loadData()
+  {
+    this.storage.get("location").then((val)=> {
+      if(val != null) {
+        this.items = val;
+      } 
+    });
+  }
+  itemSelected(item)
+  {
+    alert(item);
+  }
+  saveLocation(title)
+  {
+    var obj = { id: (new Date()).getTime(), title : title, lat : this.lat, lng : this.lng };
+    this.items.push(obj);
+    this.storage.set("location", this.items);
+  }
+  goLocation(item)
+  {
+    
+    window.open('geo:0,0?z=3&q=' + item.lat + ',' + item.lng + '(' + item.title + ')' , '_system');
+  }
+  delLocation(idx)
+  {
+    this.items.splice(idx, 1);
+    this.storage.set("location", this.items);
+  }
+  addLocation()
+  {
+    this.showLoading();
+    this.geolocation.getCurrentPosition({enableHighAccuracy:true}).then((resp) => {
 
+        this.lat = resp.coords.latitude;
+        this.lng = resp.coords.longitude;
+        var defaultTitle = "1";
+        this.nativeGeocoder.reverseGeocode(this.lat, this.lng)
+        .then((result: NativeGeocoderReverseResult) => {
+            defaultTitle = result.thoroughfare + " " + result.subThoroughfare;
+            this.popupInputName(defaultTitle); 
+            this.loading.dismiss();
+          }
+        )
+        .catch((error: any) => {  this.loading.dismiss(); alert(error);})
+        ;
+        
+      
+     }).catch((error) => {
+      this.loading.dismiss();
+       alert('Error getting location' + error);
+       
+     });
+  }
+  popupInputName(defaultTitle)
+  {
+    
+    let prompt = this.alertCtrl.create({
+      title: '위치저장',
+      message: "저장할 이름을 입력하세요.",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: '이름',
+          value : defaultTitle
+        },
+      ],
+      buttons: [
+        {
+          text: '취소',
+          handler: data => {
+            
+          }
+        },
+        {
+          text: '저장',
+          handler: data => {
+            this.saveLocation(data.title);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 }
